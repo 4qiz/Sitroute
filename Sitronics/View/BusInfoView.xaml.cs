d9393d7e6c70b4d8dc4d1d57380469a587a55757
Pluhenciya <132158810+Pluhenciya@users.noreply.github.com>
2023-11-29 20:44:59 +0300

    Грфики

--- Sitronics/View/BusInfoView.xaml.cs
+++ Sitronics/View/BusInfoView.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using LiveCharts;
+using Microsoft.EntityFrameworkCore;
+using Sitronics.Data;
+using Sitronics.Models;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Sitronics.View
 {
@@ -23,6 +14,36 @@ namespace Sitronics.View
         public BusInfoView()
         {
             InitializeComponent();
+
+            Bus currentBus = new();
+
+            using (var context = new SitrouteDataContext())
+            {
+                currentBus = context.Buses.Include(b => b.Schedules).ThenInclude(s => s.IdBusStationNavigation)
+                    .Where(b => b.IdBus == 1).First();
+            }
+
+            numberBusTextBlock.Text = "Номер:" + currentBus.Number.ToUpper();
+            countPeopleTextBlock.Text = "Людей в автобусе:" + (currentBus.Schedules.Sum(s => s.PeopleCountBoardingBus) -
+                currentBus.Schedules.Sum(s => s.PeopleCountGettingOffBus)).ToString();
+
+            ChartValues<int> peopleCountBoardingBus = new();
+            ChartValues<int> peopleCountGettingOffBus = new();
+            List<string> labels = new();
+            var shedules = currentBus.Schedules
+                .Where(s => s.PeopleCountBoardingBus != null && s.PeopleCountGettingOffBus != null).ToList();
+            int step = Convert.ToInt32(Math.Ceiling(Convert.ToDouble(Math.Max(shedules.Max(s=>s.PeopleCountBoardingBus).Value, 
+                shedules.Max(s => s.PeopleCountGettingOffBus).Value)/10)));
+            foreach (var shedule in shedules)
+            {
+                peopleCountBoardingBus.Add(shedule.PeopleCountBoardingBus ?? 0);
+                peopleCountGettingOffBus.Add(shedule.PeopleCountGettingOffBus ?? 0);
+                labels.Add(shedule.IdBusStationNavigation.Name);
+            }
+            peopleCountBoardingBusLineSeries.Values = peopleCountBoardingBus;
+            peopleCountGettingOffBusLineSeries.Values = peopleCountGettingOffBus;
+            xAxis.Labels = labels;
+            yAxis.Separator = new LiveCharts.Wpf.Separator() { Step = step };
         }
     }
 }
