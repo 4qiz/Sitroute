f1c07cd469770bd5f95de74ad8ee5b5bdbc6e7dc
Meresk <Yakovx95@mail.ru>
2023-11-30 01:14:21 +0300

    Немного градиентов на окно авторизации

--- Sitronics/App.xaml.cs
+++ Sitronics/App.xaml.cs
@@ -18,8 +18,6 @@ namespace Sitronics
                 {
                     var mainView = new MainWindow();
                     mainView.Show();
-                    
-                    
                 }
             };
         }
