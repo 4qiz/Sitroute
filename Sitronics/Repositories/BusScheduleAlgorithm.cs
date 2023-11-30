72780b4258a297ebb0e35b4c96a1163acc0bdc69
SH4R1K <0504051@mail.ru>
2023-12-01 00:12:17 +0300

    еще какая-то муть с алгоритмом, не судите строго, спать хочу

--- Sitronics/Repositories/BusScheduleAlgorithm.cs
+++ Sitronics/Repositories/BusScheduleAlgorithm.cs
@@ -1,7 +1,6 @@
-﻿using Sitronics.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.EntityFrameworkCore;
+using Sitronics.Data;
+using Sitronics.Models;
 
 namespace Sitronics
 {
@@ -16,7 +15,7 @@ namespace Sitronics
             int delay = (int)(routeTime / busCount);
             int rushTimeDelay = 3;
             int chillTime = 15;
-            DateTime busStartTime = startDate;
+            DateTime busStartTime;
             var x = 1;
             var rounds = workMinutes / (routeTime * 2);
             for (int i = 0; i < busCount; i++)
@@ -25,42 +24,40 @@ namespace Sitronics
                 for (int j = 1; j <= (workMinutes / routeTime); j++)
                 {
                     busStartTime = busStartTime.AddMinutes(IsRushTime(busStartTime) ? rushTimeDelay * i : delay * i);
-                    schedules.AddRange(MakeBusSchedule(buses[i], routeByBusStation, busStartTime, frequencyInMinutes));
-                    busStartTime = startDate.AddMinutes(routeTime*j+chillTime);
+                    schedules.AddRange(MakeBusSchedule(buses[i], routeByBusStation, busStartTime));
+                    busStartTime = startDate.AddMinutes(routeTime * j + chillTime);
                 }
             }
             return schedules;
         }
 
 
-        public List<Schedule> MakeBusSchedule(Bus bus, List<RouteByBusStation> busStops, DateTime busStartTime, int frequencyInMinutes)
+        public List<Schedule> MakeBusSchedule(Bus bus, List<RouteByBusStation> busStops, DateTime busStartTime)
         {
             List<Schedule> schedules = new List<Schedule>();
-            foreach (var busStop in busStops)
+
+            for (int i = 1; i < busStops.Count; i++)
             {
+                RouteByBusStation startBusStop = busStops[i - 1];
+                RouteByBusStation endBusStop = busStops[i];
                 var schedule = new Schedule
                 {
                     IdBus = bus.IdBus,
-                    IdBusStation = busStop.IdBusStation,
+                    IdBusStation = startBusStop.IdBusStation,
                     Time = busStartTime
                 };
                 schedules.Add(schedule);
-                busStartTime = busStartTime.AddMinutes(frequencyInMinutes); // busStartTime.AddMinute(время до остановки)
+                busStartTime = busStartTime.AddMinutes(GetIntervalInMinutesBetweenBusStations((int)bus.IdRoute, bus.IdBus, startBusStop.IdBusStation, endBusStop.IdBusStation)); // busStartTime.AddMinute(время до остановки)
             }
             return schedules;
         }
 
         private static bool IsRushTime(DateTime currentDateTime)
         {
-            var start = new TimeSpan(8,0,0);
+            var start = new TimeSpan(8, 0, 0);
             var end = new TimeSpan(10, 0, 0);
             TimeSpan now = currentDateTime.TimeOfDay;
             return (now >= start) && (now <= end);
-        }
-
-        private static bool IsRushOur(DateTime currentDateTime)
-        {
-            return Enumerable.Range(9, 12).Contains(currentDateTime.Hour) || Enumerable.Range(18, 21).Contains(currentDateTime.Hour);
         }
 
         public int CalculateBusCount(int minutesToSolveRoute, int delay)
@@ -85,6 +82,29 @@ namespace Sitronics
             // Внедрить логику, чтобы проверить, подходят ли дорожные условия для поездок на автобусе
             return true;
         }
+        private int GetIntervalInMinutesBetweenBusStations(int idRoute, int idBus, int idStartBusStation, int idEndBusStation)
+        {
+            using (var context = new SitrouteDataContext())
+            {
+                var routeByBusStations = context.RouteByBusStations
+                    .Include(s => s.IdBusStationNavigation)
+                    .ThenInclude(bs => bs.Schedules)
+                    .Where(s => s.IdRoute == idRoute)
+                    .ToList();
 
+                return Convert.ToInt32(Math.Round((
+                    GetArrivalTime(routeByBusStations, idEndBusStation, idBus) -
+                    GetArrivalTime(routeByBusStations, idStartBusStation, idBus)).TotalMinutes));
+            }
+        }
+
+        private DateTime GetArrivalTime(List<RouteByBusStation> routeByBusStations, int idBusStation, int idBus)
+        {
+            return routeByBusStations.Where(s => s.IdBusStation == idBusStation).First().IdBusStationNavigation.Schedules
+                                    .Where(s => s.IdBus == idBus)
+                                    .OrderBy(s => s.Time)
+                                    .Select(s => new { s.Time })
+                                    .First().Time;
+        }
     }
 }
