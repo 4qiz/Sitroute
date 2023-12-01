1392d0589a0ff479e65bd7826e9fd12a78b4c081
SH4R1K <0504051@mail.ru>
2023-12-01 18:28:43 +0300

    async update

--- Sitronics/View/ScheduleView.xaml.cs
+++ Sitronics/View/ScheduleView.xaml.cs
@@ -13,6 +13,9 @@ namespace Sitronics.View
         public ScheduleView()
         {
             InitializeComponent();
+        }
+        private async void StackPanel_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
             BusScheduleAlgorithm algorithm = new BusScheduleAlgorithm();
             DateTime startTime = DateTime.Parse("2022-01-02 08:00:00");
             DateTime endTime = DateTime.Parse("2022-01-02 22:00:00");
@@ -23,7 +26,7 @@ namespace Sitronics.View
                     .ThenInclude(r => r.IdBusStationNavigation)
                     .Include(r => r.Buses);
                 var buses = route.FirstOrDefault().RouteByBusStations;
-                List<Schedule> schedule = algorithm.GenerateBusSchedule(
+                List<Schedule> schedule = await Task.Run(() => algorithm.GenerateBusSchedule(
                     startTime,
                     endTime,
                     5,
@@ -31,18 +34,10 @@ namespace Sitronics.View
                     route.FirstOrDefault(r => r.IdRoute == 1).Buses.ToList(),
                     "",
                     ""
-                    );
-                List<Schedule> schedule2 = algorithm.GenerateBusSchedule(
-                    startTime,
-                    endTime,
-                    5,
-                    route.FirstOrDefault(r => r.IdRoute == 1).RouteByBusStations.Reverse().ToList(),
-                    route.FirstOrDefault(r => r.IdRoute == 1).Buses.Reverse().ToList(),
-                    "",
-                    ""
-                    );
+                    ));
                 scheduleDataGrid.ItemsSource = schedule.OrderBy(s => s.IdBusStation).ThenBy(s => s.Time);
-                //schedule2DataGrid.ItemsSource = schedule2.OrderByDescending(s => s.IdBusStation).ThenBy(s => s.Time);
+                //schedule2DataGrid.ItemsSource = schedule2.OrderByDescending(s => s.IdBusStation).ThenBy(s => s.Time); */
+
             }
         }
     }
