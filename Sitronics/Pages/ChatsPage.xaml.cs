37070769d27cd8ad3cb4439a7cc6da10a7acf178
EgorPC <egor.antipin.1995mail.ru@gmail.com>
2023-12-02 12:10:06 +0300

    chats page design

--- Sitronics/Pages/ChatsPage.xaml.cs
+++ Sitronics/Pages/ChatsPage.xaml.cs
@@ -4,6 +4,7 @@ using Sitronics.Models;
 using Sitronics.Repositories;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Sitronics.Pages
 {
@@ -66,6 +67,11 @@ namespace Sitronics.Pages
 
         private static Button GetChat(Driver? driver, Message? message = null)
         {
+            //var nameTextBlock = new TextBlock() 
+            //{
+            //    Text = $"{driver.IdDriverNavigation.SecondName} {driver.IdDriverNavigation.FirstName}" +
+            //    $" {driver.IdDriverNavigation.Patronymic}"
+            //};
             var chatTextBlock = new TextBlock
             {
                 Text = $"{driver.IdDriverNavigation.SecondName} {driver.IdDriverNavigation.FirstName}" +
@@ -76,9 +82,26 @@ namespace Sitronics.Pages
                 var today = DateTime.Now;
                 chatTextBlock.Text += $"\t{(message.Time.Date == today.Date ? message.Time.ToString("HH:mm") :
                     message.Time.Year == today.Year ? message.Time.ToString("dd MMM") : message.Time.ToString("dd MMM yyyy"))}" +
-                    $"\n{(message.IdRecipient == driver.IdDriver ? "Вы:" : "")}{message.Value}";
+                    $"\n{(message.IdRecipient == driver.IdDriver ? "Вы:" : "")} {message.Value}";
             }
-            var chatButton = new Button { Content = chatTextBlock, Style = null, Tag = driver.IdDriver };
+            var chatItemStackPanel = new StackPanel() 
+            {
+                MaxHeight = 70,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                Margin =new Thickness(5),
+            };
+            chatItemStackPanel.Children.Add(chatTextBlock);
+            var chatButton = new Button {
+                Content = chatItemStackPanel,
+                
+                Tag = driver.IdDriver,
+                //Width = 400,
+                //HorizontalAlignment = HorizontalAlignment.Left,
+                //HorizontalContentAlignment = HorizontalAlignment.Left,
+                //Background = new SolidColorBrush(Color.FromRgb(70, 69, 89)),
+                //Foreground = new SolidColorBrush(Color.FromRgb(227, 224, 249)),
+            };
+            chatButton.Style = Application.Current.Resources["chatItemButton"] as Style;
             chatButton.Click += (object sender, RoutedEventArgs e) => { Manager.MainFrame.Navigate(new CurrentChatPage(driver));};
             return chatButton;
         }
