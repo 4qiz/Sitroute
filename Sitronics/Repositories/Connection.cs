5d57faa5d5496f29ef2e413690bf8303e2551b2c
SH4R1K <0504051@mail.ru>
2023-12-02 21:53:43 +0300

    connection api

--- Sitronics/Repositories/Connection.cs
+++ Sitronics/Repositories/Connection.cs
@@ -12,7 +12,7 @@ namespace Sitronics.Repositories
     {
         public static HttpClient Client { get; set; } = new HttpClient() 
         { 
-            BaseAddress = new Uri("http://localhost:5038") 
+            BaseAddress = new Uri("https://Dimaso.bsite.net/") 
         };
 
         public static User CurrentUser { get; set; } = null;
