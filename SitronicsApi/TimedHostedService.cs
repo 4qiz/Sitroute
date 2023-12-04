5eb2e651209acc0e97db9e10280f179f05ec262a
Pluhenciya <vanya.ponomarv.00@bk.ru>
2023-12-05 00:27:39 +0300

    таймер в api

---
+++ SitronicsApi/TimedHostedService.cs
@@ -0,0 +1,25 @@
+﻿using NetTopologySuite.Geometries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SitronicsApi
+{
+    internal static class ConverterGeometry
+    {
+        public static Geometry GetPointByString(string value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            var startCoordinate = value.IndexOf("(") + 1;
+            var startSecondCoordinate = value.IndexOf(" ", startCoordinate) + 1;
+            var x = double.Parse(value.Substring(startCoordinate, startSecondCoordinate - startCoordinate - 1).Replace('.', ','));
+            var y = double.Parse(value.Substring(startSecondCoordinate, value.IndexOf(")") - startSecondCoordinate).Replace('.', ','));
+            return new Point(x, y) { SRID = 4326 };
+        }
+    }
+}
