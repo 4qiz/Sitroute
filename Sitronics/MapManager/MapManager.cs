3c6cb4dff25d7bac902874eba514fc2f450ea443
Meresk <Yakovx95@mail.ru>
2023-12-06 19:08:58 +0300

    RouteInfoView ver 1.1 and svg

--- Sitronics/MapManager/MapManager.cs
+++ Sitronics/MapManager/MapManager.cs
@@ -68,7 +68,7 @@ namespace Sitronics.MapManager
 
         public static GMapMarker CreateIncidentMarker(PointLatLng point, ref GMapControl mapView, Factor factor, GMapMarker? lastMarker = null)
         {
-            string incidentIcon = "M480 0c-17.7 0-32 14.3-32 32V192 512h64V192H624c8.8 0 16-7.2 16-16V48c0-8.8-7.2-16-16-16H512c0-17.7-14.3-32-32-32zM416 159L276.8 39.7c-12-10.3-29.7-10.3-41.7 0l-224 192C1 240.4-2.7 254.5 2 267.1S18.6 288 32 288H64V480c0 17.7 14.3 32 32 32h64c17.7 0 32-14.3 32-32V384c0-17.7 14.3-32 32-32h64c17.7 0 32 14.3 32 32v96c0 17.7 14.3 32 32 32h64.7l.2 0h-1V159z";
+            string incidentIcon = "M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z";
             SolidColorBrush fillColor;
             fillColor = Brushes.AliceBlue;
             var currentMarker = CreateMarker(point, ref mapView, incidentIcon, lastMarker, fillColor: fillColor);
